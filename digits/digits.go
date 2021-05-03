b4183b16fed7bcbfaafea1a744319454b1a6cbc3
yukinissie <bigtree2490@outlook.jp>
2021-05-03 18:32:08 +0900

    Learned about digits.

---
+++ digits/digits.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 5.2
+	b := 7.4
+	fmt.Printf("a+b=%.2f\n", a+b)
+	fmt.Printf("a-b=%.2f\n", a-b)
+	fmt.Printf("a*b=%.2f\n", a*b)
+	fmt.Printf("a/b=%.4f\n", a/b)
+}
